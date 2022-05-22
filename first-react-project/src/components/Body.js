c2ca18ddb620f0340bf885f92a1408969f722f36
unknown <prashanthdilshaan@gmail.com>
2022-05-22 19:23:43 -0400

    added
    js tags for apps.js

--- first-react-project/src/components/Body.js
+++ first-react-project/src/components/Body.js
@@ -3,7 +3,9 @@ import "../style.css"
 
 export default function Body() {
     return (
-        <>
+        <div className="body">
+
+
             <h3 className="body--titles">About</h3>
             <p className="body--info">
                 I am a frontend developer with a particular interest in making things simple and automating daily tasks. I try to keep up with security and best practices, and am always looking for new things to learn.
@@ -13,6 +15,8 @@ export default function Body() {
                 Food expert. Music scholar. Reader. Internet fanatic. Bacon buff. Entrepreneur. Travel geek. Pop culture ninja. Coffee fanatic.
             </p>
 
-        </>
+        </div>
+
+
     )
 }
